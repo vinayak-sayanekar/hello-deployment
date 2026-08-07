d7898fd6fb47c4285a52806a78bc97e210725fa7
Vinayak-Sayanekar <vinayaksayanekar@gmail.com>
2026-08-07 17:38:22 +0530

    Add MySQL service to Docker Compose

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME", 'hello_deployment'),
         "USER": os.environ.get("DB_USER", 'root'),
         "PASSWORD": os.environ.get("DB_PASSWORD", 'root'),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "HOST": os.environ.get("DB_HOST", "mysql"),
         "PORT": os.environ.get("DB_PORT", "3306"),
         'OPTIONS': {
             'charset': 'utf8mb4',
